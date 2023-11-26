4f9fd04e0e0931020e75b8c0466c561de6ada001
0-zh <1432423@fds.ru>
2023-11-27 01:39:49 +0400

    sshmethod

---
+++ app/Http/Controllers/StashController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StashController extends Controller
+{
+    public function sshGitFromLaravel2() {
+        return ['test', 'test2'];
+    }
+}
